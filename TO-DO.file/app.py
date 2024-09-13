335a04281efc9fb057d159aaaefb8ef1b1b8bbda
ManhicaThomas <thomasmanchica@gmail.com>
2024-09-13 15:01:29 +0200

    update

--- TO-DO.file/app.py
+++ TO-DO.file/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def hello_world():
